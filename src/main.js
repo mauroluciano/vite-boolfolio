47049b36389f4c764bef36a5723fb3f4b470cac1
mauroluciano <mauroluciano911@gmail.com>
2023-11-08 15:44:54 +0100

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/scss/style.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
 createApp(App).mount("#app");
