cdc43ca566cc429fef1156d3bb32e4157232b819
mauroluciano <mauroluciano911@gmail.com>
2023-11-08 16:25:01 +0100

    install axios

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script>
+import ProjectList from "./components/projects/ProjectList.vue";
+import axios from "axios";
 export default {
   data() {
     return {};
   },
+
+  components: { ProjectList },
 };
 </script>
 
 <template>
   <h1>hello world</h1>
+  <ProjectList />
 </template>
 <style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello wrod",
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>ProjectList</h2>
+  <br />
+</template>
+<style lang="scss" scoped></style>
